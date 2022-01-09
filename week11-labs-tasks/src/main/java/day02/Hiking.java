1590b765e46f0ede8c8a6a1a2227176caeb4efea
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2022-01-09 19:38:03 +0100

    Hiking task is done

---
+++ week11-labs-tasks/src/main/java/day02/Hiking.java
@@ -0,0 +1,14 @@
+package day02;
+
+import java.util.List;
+
+public class Hiking {
+    public int getPlusElevation(List<Integer> elevations) {
+        int fullElevation = 0;
+
+        for (int i = 1; i < elevations.size(); i++) {
+            fullElevation += Math.max(elevations.get(i) - elevations.get(i - 1), 0);
+        }
+        return fullElevation;
+    }
+}
