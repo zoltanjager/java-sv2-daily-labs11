ee4168456c003b187b27f22303ebcf76818abe4e
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2022-01-09 19:38:49 +0100

    Lottery task is done

---
+++ week11-labs-tasks/src/main/java/day05/Lottery.java
@@ -0,0 +1,28 @@
+package day05;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Lottery {
+    private int numberOfNumbers;
+    private int limit;
+    private Random random = new Random();
+
+    public Lottery(int numberOfNumbers, int limit) {
+        this.numberOfNumbers = numberOfNumbers;
+        this.limit = limit;
+    }
+
+    public List<Integer> startLottery() {
+        List<Integer> result = new ArrayList<>();
+
+        while (result.size() != numberOfNumbers) {
+            int actualNumber = random.nextInt(1, limit + 1);
+            if (!result.contains(actualNumber)) {
+                result.add(actualNumber);
+            }
+        }
+        return result;
+    }
+}
